5db2d56de4a15664f69e27ec17ae6250620d9ad6
kogcyc <kogcyc@gmail.com>
2025-07-07 07:36:34 -0700

    Update tissue_config.py

--- tissue_config.py
+++ tissue_config.py
@@ -9,4 +9,4 @@ STATIC_DIR = ROOT_DIR / "static"
 
 # sitemap.xml config
 sitemap_path = BUILD_DIR / "sitemap.xml"
-sitemap_base_url = "https://domain.io"  # CHANGE THIS!
+sitemap_base_url = "https://nearzero.vercel.app"  # CHANGE THIS!
